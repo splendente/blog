d29d54c5385f1cbf9e52a91a145ced227cd590a1
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-03-25 20:35:57 +0900

    :gear: set lang attribute to 'ja'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { lang: "ja" },
+    },
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/fonts", "@nuxt/content"],
   experimental: {
