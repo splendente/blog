30473484adcfdf9bda135eb3a768e225d2b67133
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-03-30 00:02:15 +0900

    :gear: install unplugin-auto-import & unplugin-vue-components for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 import { defineVitestConfig } from "@nuxt/test-utils/config";
+import Vue from "@vitejs/plugin-vue";
+import AutoImportFunctions from "unplugin-auto-import/vite";
+import AutoImportComponents from "unplugin-vue-components/vite";
 
 export default defineVitestConfig({
+  plugins: [
+    Vue(),
+    AutoImportFunctions({ imports: "vue" }),
+    AutoImportComponents({ dirs: ["components"] }),
+  ],
   test: {
     environment: "happy-dom",
   },
