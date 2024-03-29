caff5249aa5e499cafceb2e0da7b2138320de053
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-03-29 20:38:52 +0900

    :sparkles: create sort function for createdAt

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
+<script setup lang="ts">
+// 降順かどうか
+const desc = ref(true);
+
+/**
+ * 降順と昇順を切り替える
+ */
+const toggleSort = () => {
+  desc.value = !desc.value;
+};
+
+/**
+ * クエリを作成する
+ * @returns {object} クエリ
+ */
+const query = computed(() => {
+  return { createdAt: desc.value ? -1 : 1, $numeric: true };
+});
+</script>
+
 <template>
   <main>
-    <ContentList
-      path="/"
-      v-slot="{ list }"
-      :sort="{ createdAt: -1, $numeric: true }"
-    >
+    <SortMenu :desc="desc" @toggle-sort="toggleSort" class="sort-menu" />
+    <ContentList path="/" v-slot="{ list }" :sort="query">
       <Card
         v-for="(article, index) in list"
         :key="index"
@@ -20,8 +37,12 @@
 
 <style scoped>
 main {
-  display: flex;
+  display: grid;
   flex-direction: column;
   gap: 32px 0;
+}
+
+main > .sort-menu {
+  justify-self: right;
 }
 </style>
