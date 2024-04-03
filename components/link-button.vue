9e02833d4f35a45232fec054c32771fcf46afa1a
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-04 00:01:58 +0900

    :sparkles: create link-button component & change header component

---
+++ components/link-button.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+const props = defineProps({
+  isNuxtLink: {
+    type: Boolean,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: false,
+  },
+  href: {
+    type: String,
+    required: false,
+  },
+  target: {
+    type: String,
+    required: false,
+  },
+});
+
+// NuxtLinkの場合に設定する属性情報
+const nuxtLinkProps = {
+  to: props.to,
+};
+
+// aタグの場合に設定する属性情報
+const anchorProps = {
+  href: props.href,
+  target: props.target,
+};
+
+const nuxtLinkComponent = computed(() => {
+  return resolveComponent("NuxtLink");
+});
+</script>
+
+<template>
+  <component
+    class="link-button"
+    :is="isNuxtLink ? nuxtLinkComponent : 'a'"
+    v-bind="{
+      ...(isNuxtLink && nuxtLinkProps),
+      ...(!isNuxtLink && anchorProps),
+    }"
+  >
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+.link-button {
+  display: block;
+  width: fit-content;
+  height: auto;
+  padding: 4px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background-color: #fff;
+}
+
+.link-button:hover {
+  cursor: pointer;
+  background-color: #f6f6f6;
+}
+</style>
