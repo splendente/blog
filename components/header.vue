1d8f609823e8e2eb3cc4d35c52103b56928f45f4
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-04 00:01:58 +0900

    :sparkles: create link-button component & change header component

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,18 @@
 <template>
   <header>
     <NuxtLink to="/" class="title">Blog</NuxtLink>
+    <div>
+      <LinkButton
+        :is-nuxt-link="false"
+        href="https://github.com/splendente/blog"
+        target="_blank"
+      >
+        <img class="icon" src="@/assets/images/github.svg" />
+      </LinkButton>
+      <LinkButton :is-nuxt-link="false" href="/feed.xml" target="_blank">
+        <img class="icon" src="@/assets/images/rss.svg" />
+      </LinkButton>
+    </div>
   </header>
 </template>
 
@@ -8,10 +20,25 @@
 header {
   padding: 16px 24px;
   border-bottom: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 header .title {
   text-decoration: none;
   color: #3c3c3c;
+}
+
+header > div {
+  display: flex;
+  align-items: center;
+  gap: 0 8px;
+}
+
+.icon {
+  display: block;
+  width: 16px;
+  height: 16px;
 }
 </style>
