1887ac36841b1ee699e53c3afb86efa12016780b
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-10 00:09:31 +0900

    :sparkles: create year-select component

---
+++ components/year-select.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const targetYearModel = defineModel();
+</script>
+
+<template>
+  <div>
+    <select name="target-year" id="target-year" v-model="targetYearModel">
+      <option value="2024">2024年</option>
+    </select>
+  </div>
+</template>
+
+<style scoped>
+div {
+  width: fit-content;
+  height: fit-content;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+}
+
+select {
+  padding: 6px 12px;
+  font-size: 12px;
+  color: #3c3c3c;
+}
+
+select:hover {
+  cursor: pointer;
+}
+</style>
