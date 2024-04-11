ad66bfcb7551e59f65ee7261ce2d7fdde481dbf6
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-11 19:00:45 +0900

    :rotating_light: fix unit test of footer.server.vue

--- tests/components/footer.spec.ts
+++ tests/components/footer.spec.ts
@@ -1,12 +1,12 @@
 import { describe, expect, test } from "vitest";
 import { shallowMount } from "@vue/test-utils";
-import Footer from "./../../components/footer.vue";
+import Footer from "@/components/footer.server.vue";
 
 describe("Footerコンポーネント", () => {
   const footer = shallowMount(Footer);
 
   test("コピーライトが表示されている", () => {
     expect(footer.find("p").exists()).toBe(true);
-    expect(footer.find("p").text()).toBe("© 2023 Hikaru Kobayashi.");
+    expect(footer.find("p").text()).toBe("© 2024 Hikaru Kobayashi.");
   });
 });
