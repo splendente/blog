9d2a998e21e6cf960ad65fdb96d8f7a6ac81a6e5
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-13 20:55:37 +0900

    :sparkles: create tooltip component

---
+++ components/tooltip.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  top: {
+    type: Number,
+    required: true,
+  },
+  left: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div
+    v-if="isVisible && 0 < props.date.length"
+    :style="`top:${top - 24}px;left:${left - 55}px;`"
+    class="tooltip"
+  >
+    <p>{{ formatDateString(date) }}</p>
+  </div>
+</template>
+
+<style scoped>
+.tooltip {
+  position: absolute;
+  width: 110px;
+  height: fit-content;
+  background-color: #3c3c3c;
+  border: none;
+  padding: 4px 8px;
+  border-radius: 4px;
+  text-align: center;
+  white-space: nowrap;
+}
+
+.tooltip > p {
+  font-size: 12px;
+  color: #fff;
+}
+</style>
