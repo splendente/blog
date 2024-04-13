1821759f9149eb077ffa24e1fad8bbf611e1926a
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-13 23:16:21 +0900

    :gear: install storybook & related packages

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+/** @type { import('@storybook/vue3').Preview } */
+import "../assets/css/reset.css";
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
