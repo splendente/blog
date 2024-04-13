d27c59507b78a67769943ca5affbaa8db77776ba
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-13 23:41:25 +0900

    :sparkles: create stories of footer component

---
+++ stories/components/footer.stories.ts
@@ -0,0 +1,9 @@
+import footer from "@/components/footer.server.vue";
+
+export default {
+  title: "components/footer",
+  component: footer,
+  tags: ["autodocs"],
+};
+
+export const Default = {};
