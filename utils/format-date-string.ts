bf0d772bf022758867d8a8cb6136aca3058bf2cc
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-13 14:54:32 +0900

    :recycle: convert JSDoc comment from uppercase to lowercase

--- utils/format-date-string.ts
+++ utils/format-date-string.ts
@@ -1,7 +1,7 @@
 /**
  * 引数で受け取った日付を文字列に変換する
- * @param {string} date 日付
- * @returns {string} フォーマットされた状態の日付情報
+ * @param {String} date 日付
+ * @returns {String} フォーマットされた状態の日付情報
  */
 export default function (date: string): string {
   return new Date(date).toDateString();
