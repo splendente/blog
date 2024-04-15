ef275a74ec93fc0b5821dd94f003c53a912a1854
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-15 23:04:25 +0900

    :sparkles: create tag component

---
+++ components/tag.server.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <p class="tag"># {{ text }}</p>
+</template>
+
+<style scoped>
+.tag {
+  display: block;
+  width: fit-content;
+  font-size: 12px;
+  color: #3c3c3c;
+  background-color: #fff;
+  padding: 2px 8px;
+  border: 1px solid #ddd;
+  border-radius: 12px;
+}
+</style>
