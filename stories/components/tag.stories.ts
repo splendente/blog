8426f65624bac03bc39b245c1c11406980bd1504
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-16 00:09:11 +0900

    :sparkles: create stories of tag component

---
+++ stories/components/tag.stories.ts
@@ -0,0 +1,13 @@
+import Tag from "@/components/tag.server.vue";
+
+export default {
+  title: "components/tag",
+  component: Tag,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    text: "Vue.js",
+  },
+};
