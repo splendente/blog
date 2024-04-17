32fddaa1c9f9b52fa57483204e6ce87d25ced0df
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-17 20:45:08 +0900

    :rotating_light: add unit test of footer component

--- tests/components/footer.spec.ts
+++ tests/components/footer.spec.ts
@@ -1,12 +1,23 @@
 import { describe, expect, test } from "vitest";
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 import Footer from "@/components/footer.server.vue";
 
 describe("Footerコンポーネント", () => {
-  const footer = shallowMount(Footer);
+  const footer = shallowMount(Footer, {
+    global: {
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    },
+  });
 
   test("コピーライトが表示されている", () => {
     expect(footer.find("p").exists()).toBe(true);
     expect(footer.find("p").text()).toBe("© 2024 Hikaru Kobayashi.");
+  });
+
+  test("Privacy Policyというリンクが表示されている", () => {
+    expect(footer.findComponent(RouterLinkStub).exists()).toBe(true);
+    expect(footer.findComponent(RouterLinkStub).text()).toBe("Privacy Policy");
   });
 });
