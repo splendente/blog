5e585472424ab1ea885174dc7e4cf19d7c2dadd4
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-17 20:51:42 +0900

    :rotating_light: create unit test of tag component

---
+++ tests/components/tag.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "vitest";
+import { shallowMount } from "@vue/test-utils";
+import Tag from "@/components/tag.server.vue";
+
+describe("Tagコンポーネント", () => {
+  const props = {
+    text: "vitest",
+  };
+
+  const tag = shallowMount(Tag, {
+    propsData: {
+      text: props.text,
+    },
+  });
+
+  test("「vitest」と表示される", () => {
+    expect(tag.find("p").exists()).toBe(true);
+    expect(tag.find("p").text()).toBe("# " + props.text);
+  });
+});
