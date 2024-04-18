165dd45407525045b2e526894967180b5865c4a8
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-18 12:05:01 +0900

    :sparkles: create stories of year-select component

---
+++ stories/components/year-slect.stories.ts
@@ -0,0 +1,17 @@
+import YearSelect from "@/components/year-select.vue";
+
+export default {
+  title: "components/year-select",
+  component: YearSelect,
+  tags: ["autodocs"],
+  render: () => ({
+    components: { YearSelect },
+    setup() {
+      const targetYearModel = ref(2024);
+      return { targetYearModel };
+    },
+    template: '<year-select v-model="targetYearModel" />',
+  }),
+};
+
+export const Default = {};
