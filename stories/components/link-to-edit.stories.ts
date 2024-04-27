09dedaae30604b80e068293cafa7928e8f55db9c
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-27 21:49:41 +0900

    :rocket: convert page-edit-link to link-to-edit

---
+++ stories/components/link-to-edit.stories.ts
@@ -0,0 +1,13 @@
+import LinkToEdit from "@/components/link-to-edit.server.vue";
+
+export default {
+  title: "components/link-to-edit",
+  component: LinkToEdit,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    fileName: "",
+  },
+};
