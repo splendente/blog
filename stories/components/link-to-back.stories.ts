b0ee91f353ad80289a261f43ace64abb8ade2e7e
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-28 08:52:04 +0900

    :sparkles: create stories of link-to-back component

---
+++ stories/components/link-to-back.stories.ts
@@ -0,0 +1,9 @@
+import LinkToBack from "@/components/link-to-back.server.vue";
+
+export default {
+  title: "components/link-to-back",
+  component: LinkToBack,
+  tags: ["autodocs"],
+};
+
+export const Default = {};
