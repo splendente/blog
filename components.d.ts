b52e7d71c07c84a11f94e3af3e619a534396f59d
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-04-27 16:37:29 +0900

    :sparkles: create stories of page-edit-link component

--- components.d.ts
+++ components.d.ts
@@ -12,6 +12,7 @@ declare module 'vue' {
     'Footer.server': typeof import('./components/footer.server.vue')['default']
     'Header.server': typeof import('./components/header.server.vue')['default']
     LinkButton: typeof import('./components/link-button.vue')['default']
+    'PageEditLink.server': typeof import('./components/page-edit-link.server.vue')['default']
     Pager: typeof import('./components/pager.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
---
+++ stories/components/page-edit-link.stories.ts
@@ -0,0 +1,13 @@
+import PageEditLink from "@/components/page-edit-link.server.vue";
+
+export default {
+  title: "components/page-edit-link",
+  component: PageEditLink,
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    fileName: "",
+  },
+};
