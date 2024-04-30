34f7972bc5d51b013cacd35ff9e6905bf891c73c
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-04-30 20:10:39 +0900

    :sparkles: create error page

---
+++ error.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface ErrorProps {
+  error: {
+    statusCode: string;
+  };
+}
+
+defineProps<ErrorProps>();
+</script>
+
+<template>
+  <NuxtLayout>
+    <p class="status-code">{{ error.statusCode }}</p>
+  </NuxtLayout>
+</template>
+
+<style scoped>
+.status-code {
+  font-size: 32px;
+}
+</style>
