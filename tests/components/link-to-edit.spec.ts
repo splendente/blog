3161cccb5a244904d2a4078bfa87ceacc6668442
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-05-06 13:43:00 +0900

    :rotating_light: set props to link-to-edit.spec.ts

--- tests/components/link-to-edit.spec.ts
+++ tests/components/link-to-edit.spec.ts
@@ -3,7 +3,11 @@ import { shallowMount } from "@vue/test-utils";
 import LinkToEdit from "@/components/link-to-edit.vue";
 
 describe("LinkToEditコンポーネント", () => {
-  const linkToEdit = shallowMount(LinkToEdit, {});
+  const linkToEdit = shallowMount(LinkToEdit, {
+    propsData: {
+      fileName: "",
+    },
+  });
 
   test("「GitHubで編集を提案する」というリンクが表示されている", () => {
     expect(linkToEdit.find("a").exists()).toBe(true);
