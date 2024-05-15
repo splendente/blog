bc89bc04793e70acefabd78a29bab70873e5bbb9
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-05-15 22:31:31 +0900

    :sparkles: convert link-button to icon-button component

---
+++ components/icon-button.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+const props = defineProps({
+  element: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+  },
+  to: {
+    type: String,
+  },
+  href: {
+    type: String,
+  },
+  target: {
+    type: String,
+    default: "_self",
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+
+defineSlots<{
+  icon: () => HTMLImageElement;
+}>();
+
+const emit = defineEmits<{
+  (e: "onClick"): void;
+}>();
+
+const onClick = () => {
+  emit("onClick");
+};
+
+// buttonタグの場合に使用する属性情報と押下時の処理
+const buttonProps = {
+  "aria-label": props.label,
+  type: props.type,
+  onClick,
+};
+
+// aタグの場合に使用する属性情報
+const anchorProps = {
+  "aria-label": props.label,
+  href: props.href,
+  target: props.target,
+};
+
+// NuxtLinkコンポーネントの場合に使用する属性情報
+const nuxtLinkProps = {
+  "aria-label": props.label,
+  to: props.href,
+  target: props.target,
+};
+
+/**
+ * @typedef Component
+ * @property {string} element 要素名
+ * @property {object} props 設定する属性情報、押下時のイベント処理
+ *
+ */
+
+/**
+ * 何の要素としてレンダリングするか判定する
+ * @returns @type {Component} - 要素名と設定する属性情報、押下時のイベント処理
+ */
+const component = computed(() => {
+  switch (props.element) {
+    case "button":
+      return {
+        element: "button",
+        props: buttonProps,
+      };
+    case "anchor-link":
+      return {
+        element: "a",
+        props: anchorProps,
+      };
+    case "nuxt-link":
+      return {
+        element: resolveComponent("NuxtLink"),
+        props: nuxtLinkProps,
+      };
+    default: {
+      return {
+        // デフォルトではbuttonタグとして表示する
+        element: "button",
+        props: buttonProps,
+      };
+    }
+  }
+});
+</script>
+
+<template>
+  <component
+    class="icon-button"
+    :is="component.element"
+    v-bind="{ ...component.props }"
+  >
+    <slot name="icon" />
+  </component>
+</template>
+
+<style scoped>
+.icon-button {
+  display: block;
+  width: fit-content;
+  height: fit-content;
+  padding: 4px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background-color: #fff;
+  transition: all 0.5s ease;
+}
+
+.icon-button:hover {
+  cursor: pointer;
+  background-color: #f6f6f6;
+}
+</style>
---
+++ stories/components/icon-button.stories.ts
@@ -0,0 +1,54 @@
+import IconButton from "@/components/icon-button.vue";
+
+type Args = {
+  element: string;
+  type: string;
+  to: string;
+  href: string;
+  target: string;
+  label: string;
+};
+
+export default {
+  title: "components/icon-button",
+  component: IconButton,
+  tags: ["autodocs"],
+  render: (args: Args) => ({
+    components: { IconButton },
+    setup() {
+      return { args };
+    },
+    template: `
+      <IconButton v-bind="{ ...args }">
+        <template #icon>
+          <img src="/assets/images/github.svg" width="16" height="16" style="display:block;" alt="" />
+        </template>
+      </IconButton>
+    `,
+  }),
+};
+
+export const Button = {
+  args: {
+    element: "button",
+    label: "ラベル",
+  },
+};
+
+export const AnchorLink = {
+  args: {
+    element: "anchor-link",
+    href: "#",
+    target: "_self",
+    label: "ラベル",
+  },
+};
+
+export const NuxtLink = {
+  args: {
+    element: "nuxt-link",
+    to: "#",
+    target: "_blank",
+    label: "ラベル",
+  },
+};
