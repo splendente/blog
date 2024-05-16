f3077ca03c0880735634f3aa23dfec083e44b706
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-05-16 22:11:01 +0900

    :sparkles: create navigation-menu component

---
+++ components/navigation-menu.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+defineProps({
+  visible: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const links = [
+  {
+    name: "X",
+    href: "https://twitter.com/splendente_dev",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/splendente/blog",
+  },
+  {
+    name: "RSSフィード",
+    href: "/feed.xml",
+  },
+];
+</script>
+
+<template>
+  <Teleport to="body">
+    <Transition appear>
+      <nav v-if="visible" class="navigation-menu">
+        <ul>
+          <li v-for="(link, index) in links" :key="index">
+            <a :href="link.href" target="_blank">{{ link.name }}</a>
+          </li>
+        </ul>
+      </nav>
+    </Transition>
+  </Teleport>
+</template>
+
+<style scoped>
+.navigation-menu {
+  width: fit-content;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  position: fixed;
+  top: 80px;
+  right: 16px;
+  background-color: #fff;
+  z-index: calc(infinity);
+}
+
+.navigation-menu li {
+  list-style: none;
+}
+
+.navigation-menu li:not(:last-child) {
+  border-bottom: 1px solid #ddd;
+}
+
+a {
+  display: flex;
+  gap: 0 8px;
+  align-items: center;
+  justify-content: space-between;
+  text-decoration: none;
+  color: #3c3c3c;
+  font-size: 14px;
+  padding: 8px 16px;
+}
+
+a::after {
+  content: "";
+  display: block;
+  width: 16px;
+  height: 16px;
+  background: url(~/assets/images/external-link.svg) center top / cover
+    no-repeat;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
