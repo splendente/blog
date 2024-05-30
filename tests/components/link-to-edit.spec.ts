a4910442870a3296328ac47b1d4224a6b8499df9
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-05-31 08:01:47 +0900

    :rotating_light: add test of href attribute in link-to-edit component

--- tests/components/link-to-edit.spec.ts
+++ tests/components/link-to-edit.spec.ts
@@ -3,14 +3,23 @@ import { shallowMount } from "@vue/test-utils";
 import LinkToEdit from "@/components/link-to-edit.vue";
 
 describe("LinkToEditコンポーネント", () => {
+  const baseUrl = "https://github.com/splendente/blog/tree/main/content/";
+  const props = {
+    fileName: "vitest",
+  };
+
   const linkToEdit = shallowMount(LinkToEdit, {
     propsData: {
-      fileName: "",
+      ...props,
     },
   });
 
   test("「GitHubで編集を提案する」というリンクが表示されている", () => {
     expect(linkToEdit.find("a").exists()).toBe(true);
     expect(linkToEdit.find("a").text()).toBe("GitHubで編集を提案する");
+  });
+
+  test(`遷移先が ${baseUrl + props.fileName} である`, () => {
+    expect(linkToEdit.attributes("href")).toBe(baseUrl + props.fileName);
   });
 });
