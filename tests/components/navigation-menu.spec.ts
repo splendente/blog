866074a8e5a35a6a57cf2a0fbb8e327faef5c723
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-06-01 14:18:12 +0900

    :rotating_light: create unit test of navigation-menu component

---
+++ tests/components/navigation-menu.spec.ts
@@ -0,0 +1,31 @@
+import { describe, expect, test } from "vitest";
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
+import NavigationMenu from "@/components/navigation-menu.vue";
+
+describe("NavigationMenuコンポーネント", () => {
+  const props = {
+    visible: true,
+  };
+  const links = getLinks();
+
+  const navigationMenu = shallowMount(NavigationMenu, {
+    propsData: {
+      ...props,
+    },
+    global: {
+      stubs: {
+        NuxtLink: RouterLinkStub,
+      },
+    },
+  });
+
+  test("リンク情報と表示されているテキストおよび遷移先が一致する", () => {
+    expect(() => {
+      navigationMenu.findAll("a").forEach((item) => {
+        links.every((link) => {
+          link.name === item.text() && link.href === item.attributes("href");
+        });
+      });
+    }).toBeTruthy();
+  });
+});
