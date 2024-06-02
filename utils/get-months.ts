92e03338a20b3db57be1687121a1bb18701ccf97
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-06-03 06:12:31 +0900

    :recycle: change to v-for process the months and days

---
+++ utils/get-months.ts
@@ -0,0 +1,19 @@
+/**
+ * 1月から12月までの月を英語で返却する
+ * @returns {Array} 1月から12月
+ */
+export default function (): string[] {
+  return [
+    "Jan",
+    "Feb",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+}
