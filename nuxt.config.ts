65fe7b998c556426e4ce983edc282221eda8a129
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-06-09 22:36:01 +0900

    :gear: set document driven mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
   css: ["~/assets/css/reset.css", "~/assets/css/markdown.css"],
   content: {
     highlight: { theme: "github-light" },
+    documentDriven: true,
   },
   site: {
     url: "https://splendente.vercel.app",
