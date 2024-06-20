78d2c5e92800cd585dff74243a21d9d7a900d426
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-06-20 22:05:50 +0900

    :rotating_light: create a11y test for index page

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("/index", () => {
+  test("WCAG2.1に基づいたアクセシビリティテスト", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    console.log(accessibilityScanResults);
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+
   test("「Home」というタイトルが設定されている", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await expect(page).toHaveTitle(/Home/);
