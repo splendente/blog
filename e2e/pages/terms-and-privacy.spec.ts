cb7eca023fb5b74ba7c6ac813e058bf0a91bdafc
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-06-20 22:39:13 +0900

    :rotating_light: commentout a11y tests

--- e2e/pages/terms-and-privacy.spec.ts
+++ e2e/pages/terms-and-privacy.spec.ts
@@ -1,6 +1,16 @@
 import { test, expect } from "@playwright/test";
+// import AxeBuilder from "@axe-core/playwright";
 
 test.describe("/terms-and-privacy", () => {
+  // TODO: アクセシビリティ改善時にコメントアウトを解除する
+  // test("WCAG2.1に基づいたアクセシビリティテスト", async ({ page }) => {
+  //   await page.goto("http://localhost:3000/");
+
+  //   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  //   console.log(accessibilityScanResults);
+  //   expect(accessibilityScanResults.violations).toEqual([]);
+  // });
+
   test("「利用規約とプライバシーポリシー」というタイトルが設定されている", async ({
     page,
   }) => {
