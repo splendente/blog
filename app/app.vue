9818c3b5a2e1cf33003bbf95aac6b449e2a8ecc7
splendente <hikaru.kobayashi0719+develop@gmail.com>
2024-07-04 00:09:41 +0900

    :rocket: add useSeoMeta in app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Hikaru Kobayashi's blog",
+  ogTitle: "Hikaru Kobayashi's blog",
+  description: "This is my personal tech blog.",
+  ogDescription: "This is my personal tech blog.",
+  twitterCard: "summary_large_image",
+});
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
