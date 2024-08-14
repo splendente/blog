821ee9d558b2dfcb2f4f8d8650b7a3b86dc46734
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-08-14 14:07:16 +0900

    :rocket: add SortMenu & sort function in tag page

--- app/pages/tags/[slug].vue
+++ app/pages/tags/[slug].vue
@@ -12,19 +12,26 @@ defineOgImageComponent("NuxtSeo", {
   title: title,
 });
 
-const query: QueryBuilderParams = {
-  path: "/",
-  where: [{ tags: { $contains: route.params.slug } }],
-  sort: [{ createdAt: -1 }],
-};
+const { desc, toggleSort } = useSort();
+
+const query: QueryBuilderParams = computed(() => {
+  return {
+    path: "/",
+    where: [{ tags: { $contains: route.params.slug } }],
+    sort: [{ createdAt: desc.value ? -1 : 1 }],
+  };
+});
 </script>
 
 <template>
   <main>
-    <h1 class="title">
-      <span>{{ route.params.slug }}</span>
-      の記事一覧
-    </h1>
+    <div class="title-wrapper">
+      <h1 class="title">
+        <span>{{ route.params.slug }}</span>
+        の記事一覧
+      </h1>
+      <SortMenu :desc="desc" class="sort-menu" @toggle-sort="toggleSort" />
+    </div>
     <ContentList v-slot="{ list }" :query="query">
       <Card
         v-for="(article, index) in list"
@@ -48,8 +55,20 @@ main {
   gap: 32px 0;
 }
 
+.title-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 .title {
   font-size: 24px;
   color: #3c3c3c;
+}
+
+@media (width < 640px) {
+  .title {
+    font-size: 20px;
+  }
 }
 </style>
