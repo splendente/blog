9c1fc6f6e12d65cfbd551214cfb56a3dc03400b7
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-08-14 23:25:41 +0900

    :sparkles: create story of search-input

---
+++ stories/components/search-input.stories.ts
@@ -0,0 +1,9 @@
+import SearchInput from "@/components/search-input.vue";
+
+export default {
+  title: "components/search-input",
+  component: SearchInput,
+  tags: ["autodocs"],
+};
+
+export const Default = {};
