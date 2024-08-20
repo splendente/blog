0e3931ddcf54938d24bbddbdef6f222a2554ac70
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-08-20 23:00:21 +0900

    :rotating_light: add a11y test in e2e

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -1,15 +1,17 @@
 import { test, expect } from "@playwright/test";
-// import AxeBuilder from "@axe-core/playwright";
+import { AxeBuilder } from "@axe-core/playwright";
 
 test.describe("/index", () => {
-  // TODO: アクセシビリティ改善時にコメントアウトを解除する
-  // test("WCAG2.1に基づいたアクセシビリティテスト", async ({ page }) => {
-  //   await page.goto("http://localhost:3000/");
-
-  //   const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-  //   console.log(accessibilityScanResults);
-  //   expect(accessibilityScanResults.violations).toEqual([]);
-  // });
+  test("WCAGの基準に基づいたアクセシビリティテスト", async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+      .analyze();
+    accessibilityScanResults.violations.forEach((result) => {
+      console.log(result.description);
+    });
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 
   test("「ホーム」というタイトルが設定されている", async ({ page }) => {
     await page.goto("http://localhost:3000/");
