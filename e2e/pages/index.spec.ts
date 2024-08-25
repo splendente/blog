bae168422f229f4b57ecb698ecd67ed5423b7566
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-08-25 10:59:47 +0900

    :rotating_light: add test to index page test

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -1,8 +1,20 @@
 import { test, expect } from "@playwright/test";
+import { BASE_URL } from "../consts";
 
-test.describe("/index", () => {
-  test("「ホーム」というタイトルが設定されている", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
-    await expect(page).toHaveTitle(/ホーム/);
-  });
+test("「ホーム」というタイトルが設定されていること", async ({ page }) => {
+  await page.goto(BASE_URL);
+  await expect(page).toHaveTitle(/ホーム/);
+});
+
+test("記事の順番が昇順に並び替わること", async ({ page }) => {
+  await page.goto(BASE_URL);
+  await page.getByRole("button", { name: "昇順に並び替える" }).click();
+});
+
+test("全ての記事の一覧へ遷移すること", async ({ page }) => {
+  await page.goto(BASE_URL);
+  await page.getByRole("link", { name: "すべての記事を見る" }).click();
+  await page.waitForURL(`${BASE_URL}/blog`);
+  await expect(page).toHaveURL(`${BASE_URL}/blog`);
+  await expect(page).toHaveTitle(/記事の一覧/);
 });
