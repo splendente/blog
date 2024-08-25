8a0bdfc3f42c4199cf6dd386893aa6ab5122bbe6
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-08-25 11:16:55 +0900

    :recycle: refactor a11y.spec.ts

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -1,26 +1,27 @@
 import { test, expect } from "@playwright/test";
 import { AxeBuilder } from "@axe-core/playwright";
+import { BASE_URL } from "./consts";
 
 const testCases = [
-  "/",
-  "/released-on-nuxt-v3-11",
-  "/blog",
-  "/tags",
-  "/tags/Nuxt",
-  "/terms-and-privacy",
+  { path: "/", title: "ホーム" },
+  { path: "/blog", title: "すべての記事一覧" },
+  { path: "/tags", title: "タグ一覧" },
+  { path: "/tags/Nuxt", title: "Nuxtの記事一覧" },
+  { path: "/released-on-nuxt-v3-11", title: "個別のページ" },
+  { path: "/terms-and-privacy", title: "利用規約とプライバシーポリシー" },
 ];
 
-testCases.forEach((path) => {
-  test(`${path} › WCAGの基準に基づいたアクセシビリティテスト`, async ({
-    page,
-  }) => {
-    await page.goto(`http://localhost:3000${path}`);
-    const accessibilityScanResults = await new AxeBuilder({ page })
-      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
-      .analyze();
-    accessibilityScanResults.violations.forEach((result) => {
-      console.log(result.description);
+test.describe("WCAGの基準に基づいたアクセシビリティテスト", () => {
+  for (const { path, title } of testCases) {
+    test(`${title}`, async ({ page }) => {
+      await page.goto(`${BASE_URL}${path}`);
+      const accessibilityScanResults = await new AxeBuilder({ page })
+        .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+        .analyze();
+      accessibilityScanResults.violations.forEach((result) => {
+        console.log(result.description);
+      });
+      expect(accessibilityScanResults.violations).toEqual([]);
     });
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+  }
 });
