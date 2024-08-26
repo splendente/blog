b0505216cc5b1d6f90451981c9f61e8a148d74f9
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-08-26 23:35:36 +0900

    :rotating_light: create report if a11y test faild

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -1,27 +1,33 @@
 import { test, expect } from "@playwright/test";
 import { AxeBuilder } from "@axe-core/playwright";
 import { BASE_URL } from "./consts";
+import { createHtmlReport } from "axe-html-reporter";
 
 const testCases = [
-  { path: "/", title: "ホーム" },
-  { path: "/blog", title: "すべての記事一覧" },
-  { path: "/tags", title: "タグ一覧" },
-  { path: "/tags/Nuxt", title: "Nuxtの記事一覧" },
-  { path: "/released-on-nuxt-v3-11", title: "個別のページ" },
-  { path: "/terms-and-privacy", title: "利用規約とプライバシーポリシー" },
+  { path: "", title: "ホーム" },
+  { path: "blog", title: "すべての記事一覧" },
+  { path: "tags", title: "タグ一覧" },
+  { path: "tags/Nuxt", title: "Nuxtの記事一覧" },
+  { path: "released-on-nuxt-v3-11", title: "個別のページ" },
+  { path: "terms-and-privacy", title: "利用規約とプライバシーポリシー" },
 ];
 
-test.describe("WCAGの基準に基づいたアクセシビリティテスト", () => {
-  for (const { path, title } of testCases) {
+for (const { path, title } of testCases) {
+  test.describe("WCAGの基準に基づいたアクセシビリティテスト", () => {
     test(`${title}`, async ({ page }) => {
-      await page.goto(`${BASE_URL}${path}`);
-      const accessibilityScanResults = await new AxeBuilder({ page })
+      await page.goto(`${BASE_URL}/${path}`);
+      const results = await new AxeBuilder({ page })
         .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
         .analyze();
-      accessibilityScanResults.violations.forEach((result) => {
-        console.log(result.description);
-      });
-      expect(accessibilityScanResults.violations).toEqual([]);
+
+      if (results.violations.length > 0) {
+        createHtmlReport({
+          results,
+          options: { outputDir: `artifacts/${path}` },
+        });
+      }
+
+      expect(results.violations).toEqual([]);
     });
-  }
-});
+  });
+}
