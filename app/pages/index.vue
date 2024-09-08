e818efdc134a48b31c7df4331605080b7b6b8107
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-09-08 10:05:26 +0900

    :art: change layouts

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,31 +1,38 @@
 <script setup lang="ts">
-import type { QueryBuilderParams } from "@nuxt/content/dist/runtime/types";
+import type { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+
+const route = useRoute()
+const title = route.query.tag ? `${route.query.tag}` : 'すべて'
 
 useHead({
-  title: "ホーム",
-});
+  title: `${title}の記事一覧`,
+})
 
-defineOgImageComponent("NuxtSeo", {
-  title: "Home",
-});
+defineOgImageComponent('NuxtSeo', {
+  title: `${title}の記事一覧`,
+})
+
+const { desc, toggleSort } = useSort()
 
 const query: QueryBuilderParams = computed(() => {
   return {
-    path: "/",
-    sort: [{ createdAt: -1 }],
-    limit: 4,
-  };
-});
+    path: '/',
+    where: [{ tags: route.query.tag && { $contains: route.query.tag } }],
+    sort: [{ createdAt: desc.value ? -1 : 1 }],
+  }
+})
 
-const date = new Date();
-const targetYear = ref<number>(date.getFullYear());
+const date = new Date()
+const targetYear = ref<number>(date.getFullYear())
 
 /**
  * contentディレクトリ配下のコンテンツの全ての情報を取得する
  */
-const { data: page } = await useAsyncData("articles", () =>
-  queryContent("/").find(),
-);
+const { data: page } = await useAsyncData('articles', () =>
+  queryContent('/')
+    .where({ tags: route.query.tag && { $contains: route.query.tag } })
+    .find(),
+)
 
 /**
  * コンテンツの作成日と更新日を取得する
@@ -33,51 +40,67 @@ const { data: page } = await useAsyncData("articles", () =>
  */
 const activeDates = computed(() => {
   // 作成日と更新日を格納する配列
-  const createdAtAndUpdatedAt: string[] = [];
+  const createdAtAndUpdatedAt: string[] = []
 
   // コンテンツが存在した場合のみ実行
   if (page.value && 0 < page.value.length) {
     page.value.forEach((obj) => {
       Object.keys(obj).forEach((key) => {
-        if (key === "createdAt" || key === "updatedAt") {
+        if (key === 'createdAt' || key === 'updatedAt') {
           // 既に同一稼働日がある場合、配列への追加は行わない
           if (!createdAtAndUpdatedAt.includes(obj[key])) {
             // 重複しない場合、配列に日付を追加する
-            createdAtAndUpdatedAt.push(obj[key]);
+            createdAtAndUpdatedAt.push(obj[key])
           }
         }
-      });
-    });
+      })
+    })
   }
 
-  return createdAtAndUpdatedAt;
-});
+  return createdAtAndUpdatedAt
+})
 </script>
 
 <template>
   <main>
-    <hgroup>
-      <h1>Welcome to my Blog.</h1>
-      <p>フロントエンドに関する情報を発信しています。</p>
-    </hgroup>
-    <YearSelect v-model="targetYear" class="year-select" />
-    <CalendarHeatmap
-      :target-year="Number(targetYear)"
-      :active-dates="activeDates"
-    />
-    <ContentList v-slot="{ list }" :query="query">
-      <Card
-        v-for="(article, index) in list"
-        :key="index"
-        :to="article._path"
-        :emoji="article.emoji"
-        :title="article.title"
-        :description="article.description"
-        :created-at="article.createdAt"
-        :tags="article.tags"
+    <div class="heatmap">
+      <YearSelect
+        v-model="targetYear"
+        class="year-select"
       />
-    </ContentList>
-    <NuxtLink to="/blog" class="link-to-blog">すべての記事を見る</NuxtLink>
+      <CalendarHeatmap
+        :target-year="Number(targetYear)"
+        :active-dates="activeDates"
+        :title="title"
+      />
+    </div>
+    <div class="title-wrapper">
+      <h1 class="title">
+        {{ title }}
+        <span>の記事一覧</span>
+      </h1>
+      <SortMenu
+        :desc="desc"
+        @toggle-sort="toggleSort"
+      />
+    </div>
+    <div class="lists">
+      <ContentList
+        v-slot="{ list }"
+        :query="query"
+      >
+        <Card
+          v-for="(article, index) in list"
+          :key="index"
+          :to="article._path"
+          :emoji="article.emoji"
+          :title="article.title"
+          :description="article.description"
+          :created-at="article.createdAt"
+          :tags="article.tags"
+        />
+      </ContentList>
+    </div>
   </main>
 </template>
 
@@ -85,49 +108,53 @@ const activeDates = computed(() => {
 main {
   flex: 1;
   width: 100%;
-  max-width: 840px;
+  max-width: 1216px;
   margin: 0 auto;
   padding: 32px 16px;
 }
 
-hgroup {
-  text-align: center;
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 64px 24px;
+.heatmap {
+  margin-bottom: 48px;
 }
 
-hgroup > h1 {
+.heatmap .year-select {
+  margin-left: auto;
+  margin-bottom: 24px;
+}
+
+.title-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 48px;
+}
+
+.title {
   font-size: 32px;
   color: #3c3c3c;
-  margin-top: 8px;
-  margin-bottom: 8px;
 }
 
-hgroup > p {
-  font-size: 14px;
+.title > span {
+  font-size: 16px;
   color: #3c3c3c;
+  margin-left: 8px;
 }
 
-main {
+.lists {
   display: grid;
-  flex-direction: column;
-  gap: 32px 0;
+  gap: 32px;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
 }
 
-main > .sort-menu,
-main > .year-select {
-  justify-self: right;
+@media (width < 768px) {
+  .lists {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
 }
 
-.link-to-blog,
-.link-to-blog:active,
-.link-to-blog:visited {
-  font-size: 14px;
-  text-align: center;
-  color: #3c3c3c;
-  display: block;
-  width: fit-content;
-  margin: 0 auto;
+@media (width < 576px) {
+  .lists {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
 }
 </style>
