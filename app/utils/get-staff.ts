7597e65c88edaf2aa618033ae8ef2434386e2aef
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2024-11-11 22:34:06 +0900

    :sparkles: add staff information

--- app/utils/get-staff.ts
+++ app/utils/get-staff.ts
@@ -1,6 +1,11 @@
 export default function () {
   return [
     {
+      name: 'TSKaigi2025',
+      url: '#',
+      date: '2025-?-?',
+    },
+    {
       name: 'Vue Fes Japan 2024',
       url: 'https://vuefes.jp/2024/staffs/hikaru-kobayashi/share',
       date: '2024-10-19',
