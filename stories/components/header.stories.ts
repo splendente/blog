5cb0e9af86467cab54f25d7004a013b91b6036a0
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-01-05 23:09:55 +0900

    :bug: mock  in Storybook (#82)

--- stories/components/header.stories.ts
+++ stories/components/header.stories.ts
@@ -4,6 +4,25 @@ export default {
   title: 'components/header',
   component: Header,
   tags: ['autodocs'],
+  render: () => ({
+    components: { Header },
+    setup() {
+      const colorMode = {
+        value: 'light',
+        preference: 'light',
+        unknown: false,
+        forced: false,
+      }
+      const app = getCurrentInstance()?.appContext.app
+      if (app) {
+        app.config.globalProperties.$colorMode = colorMode
+      }
+      return {}
+    },
+    template: `
+      <Header />
+    `,
+  }),
 }
 
 export const Default = {}
