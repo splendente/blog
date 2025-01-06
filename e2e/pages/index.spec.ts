a97bd96945934f0a077e2821781d07c6668041b8
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-01-06 23:15:29 +0900

    Revert ":rotating_light: add waitForLoadState before execute expect() (#101)"
    This reverts commit 9b5d260b64e0c1790683ef8e7e69c8683bac4acd.

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -3,6 +3,5 @@ import { BASE_URL } from '../consts'
 
 test('「すべての記事一覧」というタイトルが設定されていること', async ({ page }) => {
   await page.goto(BASE_URL)
-  await page.waitForLoadState('domcontentloaded')
   await expect(page).toHaveTitle(/すべての記事一覧/)
 })
