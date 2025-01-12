12660a5066e8c1fa1d9a35323bf4589293c557dd
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-01-12 21:23:32 +0900

    :rotating_light: change waitForLoadState to waitForFunction (#101)

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -3,6 +3,7 @@ import { BASE_URL } from '../consts'
 
 test('「すべての記事一覧」というタイトルが設定されていること', async ({ page }) => {
   await page.goto(BASE_URL)
-  await page.waitForLoadState('load')
-  await expect(page).toHaveTitle(/すべての記事一覧/)
+  await page.waitForFunction(() => document.title.includes('すべての記事一覧'))
+  const title = await page.title()
+  expect(title).toContain('すべての記事一覧')
 })
