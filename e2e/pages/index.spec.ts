ce2dbe0b101adaa74b852e46ac271f1a853f1849
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-01-12 18:56:54 +0900

    :rotating_light: add waitForURL for e2e/pages/index.spec.ts (#101)

--- e2e/pages/index.spec.ts
+++ e2e/pages/index.spec.ts
@@ -3,5 +3,6 @@ import { BASE_URL } from '../consts'
 
 test('「すべての記事一覧」というタイトルが設定されていること', async ({ page }) => {
   await page.goto(BASE_URL)
+  await page.waitForURL(`${BASE_URL}`)
   await expect(page).toHaveTitle(/すべての記事一覧/)
 })
