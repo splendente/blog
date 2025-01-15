792275db9dc6c1d49b49c099301574fa013c1c09
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-01-15 20:44:48 +0900

    :rotating_light: delete faild file (#101)

--- e2e/pages/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test'
-import { BASE_URL } from '../consts'
-
-test('「すべての記事一覧」というタイトルが設定されていること', async ({ page }) => {
-  await page.goto(BASE_URL)
-  await page.waitForFunction(() => document.title.includes('すべての記事一覧'))
-  const title = await page.title()
-  expect(title).toContain('すべての記事一覧')
-})
