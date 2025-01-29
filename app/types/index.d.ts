42abb09608b24e877884bf33c9a4d3921104142d
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-01-29 22:45:48 +0900

    :sparkles: mock () for Storybook (#109)

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -3,3 +3,21 @@ export type NavigationMenu = {
   text: string
   to?: string
 }
+
+export type TocProps = {
+  items: {
+    title: string
+    searchDepth: number
+    links: {
+      id: string
+      depth: number
+      text: string
+    }[]
+  }
+}
+
+export type CalendarHeatmapProps = {
+  targetYear: number
+  activeDates: string[]
+  title: string
+}
