06a8b70dc2d33c4c5d43c0952706e5706b002924
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-04-23 23:54:03 +0900

    :bug: fix footer test

--- app/components/footer/index.spec.ts
+++ app/components/footer/index.spec.ts
@@ -2,6 +2,8 @@ import { describe, expect, test } from 'vitest'
 import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import Footer from '@/components/footer/index.vue'
 
+const thisYear = new Date().getFullYear()
+
 describe('Footerコンポーネント', () => {
   const footer = shallowMount(Footer, {
     global: {
@@ -13,7 +15,7 @@ describe('Footerコンポーネント', () => {
 
   test('コピーライトが表示されている', () => {
     expect(footer.find('p').exists()).toBe(true)
-    expect(footer.find('p').text()).toBe('© 2024 Hikaru Kobayashi.')
+    expect(footer.find('p').text()).toBe(`© 2024 - ${thisYear} Hikaru Kobayashi.`)
   })
 
   test('「Terms and Privacy」というリンクが表示されている', () => {
